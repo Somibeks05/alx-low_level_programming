c7cb8a07be5982da0b0500a3940354d574d30b07
Somibeks05 <somibeks@yahoo.co.uk.com>
2022-10-18 08:45:45 -0700

    corrected file 12 and 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,46 @@
 #include "main.h"
 
 /**
- * main - Prints natural numbers below 1024 that are
- * multiplies of 3 or 5
+ * print_times_table - Prints the n times table
  *
- * Return: Always 0.
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
-int main(void)
+void print_times_table(int n)
 {
-int a, b;
+int a, b, op;
 
-for (a = 1; a < 1024; a++)
+if (n >= 0 && n <= 15)
 {
-if ((a % 3) == 0 || (a % 5) == 0)
-b += a;
+for (a = 0; a <= n; a++)
+{
+_putchar(48);
+for (b = 1; b <= n; b++)
+{
+op = a * b;
+_putchar(44);
+_putchar(32);
+if (op <= 9)
+{
+_putchar(32);
+_putchar(32);
+_putchar(op + 48);
 }
-printf("%d\n", b);
-return (0);
+else if (op <= 99)
+{
+_putchar(32);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
+}
+else
+{
+_putchar(((op / 100) % 10) + 48);
+_putchar(((op / 10) % 10) + 48);
+_putchar((op % 10) + 48);
+}
+}
+_putchar('\n');
+}
+}
 }
